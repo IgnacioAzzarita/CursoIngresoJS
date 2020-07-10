bf1087f25aab09500f998ce680aaf8627dd9558a
IgnacioAzzarita <68014520+IgnacioAzzarita@users.noreply.github.com>
2020-07-10 11:52:38 -0300

    Sale

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -3,7 +3,8 @@ Debemos lograr tomar un dato por 'PROMPT'
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
-	alert("ok");
+{ var nombreingresado=prompt("Ingrese su nombre");
+nombreingresado=txtIdNombre.value;
+	alert(nombreingresado);
 }
 
