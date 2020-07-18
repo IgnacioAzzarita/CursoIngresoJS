b63cd0d521d7d6e6b43c026322c54ef7a8e0d4ff
IgnacioAzzarita <68014520+IgnacioAzzarita@users.noreply.github.com>
2020-07-18 12:37:31 -0300

    Hice ejercicios 1 - 8

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,13 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad=parseInt(document.getElementById("txtIdEdad").value);
+	let estadoCivil=document.getElementById("estadoCivil").value;
 	
+	if (edad<=18 && estadoCivil != "Soltero")
+	{
+	//tomo la edad  
+	alert("Es muy pequeño para NO ser soltero");
+	}
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,7 +1,20 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad=parseInt(document.getElementById("txtIdEdad").value);
+	let estadoCivil=document.getElementById("estadoCivil").value;
+
+	if (edad>=18 && estadoCivil !="Soltero")
+	{//tomo la edad  
+	
+	}
+	else if (estadoCivil=="Soltero" && edad>=19)
+	{
+		alert("Es soltero y no es menor")
+	}
+	else
+	{
+		
+	}
 	
 
 
