7ad7e8db73db8313348f4bf9ae0217150e8fb828
IgnacioAzzarita <68014520+IgnacioAzzarita@users.noreply.github.com>
2020-07-18 14:43:12 -0300

    Hice ejercicios 1-10

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,7 @@
 function mostrar()
 {
+	let aleatorio= Math.floor(Math.random()*10);
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	alert(aleatorio);	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,17 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let aleatorio= Math.floor(Math.random()*10);
+	if (aleatorio>=9)
+	{//Genero el número RANDOM entre 1 y 10 
+	alert("La nota es "+aleatorio+" EXCELENTE");
+	}
+	else if (aleatorio>4)
+	{
+		alert("La nota es "+aleatorio+" APROBÓ");
+	}
+	else
+	{
+		alert("La nota es "+aleatorio+" Vamos,la proxima se puede");
+	}
 
 }//FIN DE LA FUNCIÓN
