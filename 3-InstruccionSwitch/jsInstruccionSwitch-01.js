3cc09fa94e94934c41901fdbca6506a771c03523
IgnacioAzzarita <68014520+IgnacioAzzarita@users.noreply.github.com>
2020-07-21 11:54:14 -0300

    Hice ejercicios 1-3

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,39 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes= document.getElementById("txtIdMes").value;
+	
+	switch(mes){
+		case "Enero":
+			alert("Que comiences bien el año!!!");
+			break;
+			case "Marzo":
+				alert("A clases!!!");
+				break;
+				case "Julio":
+					alert("Se vienen las vacaciones");
+					break;
+					case "Diciembre":
+						alert("Felices fiestas!!!");
+						break;
+	}
+	}
 
 
 
-}//FIN DE LA FUNCIÓN
+//FIN DE LA FUNCIÓN
+
+//tomo el mes
+	/*let mes=document.getElementById("txtIdMes").value;
+	if (mes=="Enero"){
+	alert("Que comiences bien el año!!!");
+	}
+	else if (mes=="Marzo"){
+		alert("A clases!!!");
+	}
+	else if (mes=="Julio"){
+		alert("Se vienen las vacaciones!!!");
+	}
+	else if (mes=="Diciembre"){
+		alert("Felices fiestas!");
+	}
+	*/
