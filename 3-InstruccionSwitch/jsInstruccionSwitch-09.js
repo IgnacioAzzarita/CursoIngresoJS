8bf5a06256b45861fd8553668cddba86803c2770
IgnacioAzzarita <68014520+IgnacioAzzarita@users.noreply.github.com>
2020-07-22 21:41:07 -0300

    Hice ejercicios 1-10

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,65 @@
-function mostrar()
-{
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+function mostrar() {
+    let estacionIngresada = document.getElementById("txtIdEstacion").value;
+	let destinoIngresado = document.getElementById("txtIdDestino").value;
+	let precioBase = 15000;
+	let precioFinal = 0;
+	let plusVariable = 0;
+	switch(estacionIngresada){
+		case "Invierno":
+			switch(destinoIngresado){
+				case "Bariloche":
+					plusVariable=precioBase*0.20;
+					precioFinal= precioBase+plusVariable;
+					alert("El costo por su estadía es de $"+precioFinal);
+					break;
+				case "Mar del plata":
+					plusVariable=precioBase*0.20;
+					precioFinal=precioBase-plusVariable;
+					alert("El costo por su estadía es de $"+precioFinal);
+					break;
+				case "Cordoba":
+				case "Cataratas":
+					plusVariable=precioBase*0.10;
+					precioFinal=precioBase-plusVariable;
+					alert("El costo por su estadía es de $"+precioFinal);
+					break;
+			}
+			break;
+		case "Verano":
+			switch(destinoIngresado){
+				case"Bariloche":
+				plusVariable=precioBase*0.20;
+				precioFinal=precioBase-plusVariable;
+				alert("El costo por su estadía es de $"+precioFinal);
+				break;
+				case "Mar del plata":
+				plusVariable=precioBase*0.20;
+				precioFinal=precioBase+plusVariable;
+				alert("El costo por su estadía es de $"+precioFinal);
+				break;
+				case "Cordoba":
+				case "Cataratas":
+				plusVariable=precioBase*0.10;
+				precioFinal=precioBase-plusVariable;
+				alert("El costo por su estadía es de $"+precioFinal);
+				break;
+			}
+			break;
+			case"Primavera":
+			case"Otoño":
+			switch(destinoIngresado){
+				case "Bariloche":
+				case "Mar del plata":
+				case "Cataratas":
+				plusVariable=precioBase*0.10;
+				precioFinal=precioBase+plusVariable;
+				alert("El costo por su estadía es de $"+precioFinal);
+				break;
+				case "Cordoba":
+					alert("El costo por su estadía es de $"+precioFinal);
+					break
+			}
+			break;
 
-}//FIN DE LA FUNCIÓN
+			}
+		}
--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,45 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
-
-}//FIN DE LA FUNCIÓN
+	let estacionIngresada=document.getElementById("txtIdEstacion").value;
+	let destinoIngresado=document.getElementById("txtIdDestino").value;
+	switch(estacionIngresada){
+		case "Invierno":
+		switch(destinoIngresado){
+		case "Bariloche":
+			alert("Se viaja");
+			break;
+		default:
+			alert("No se viaja");
+			break;
+		}
+		break;
+		case "Verano":
+			switch(destinoIngresado){
+				case "Bariloche":
+				case "Cordoba":
+					alert("No se viaja");
+					break;
+				default:
+					alert("Se viaja");
+					break;
+			}
+				break;
+		case "Otoño":
+			alert("Se viaja");
+			break;
+		case "Primavera":
+			switch(destinoIngresado){
+				case"Bariloche":
+				alert("No se viaja");
+				break;
+				default:
+					alert("Se viaja");
+					break;
+			}
+			break;
+		
+		
+	
+	}
+	}
+//FIN DE LA FUNCIÓN
