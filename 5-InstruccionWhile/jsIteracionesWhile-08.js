65dc226fd02f8b4aee741b5d82a5b016b12c434e
IgnacioAzzarita <68014520+IgnacioAzzarita@users.noreply.github.com>
2020-07-30 13:23:24 -0300

    Ejercicios While

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -3,17 +3,35 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.*/
 function mostrar()
 {
-	var contador;
-	var respuesta;
-	var sumaPositivos;
-	var multiplicacionNegativos;
-	contador=0;
+	let numero;
+	let respuesta;
+	let sumaPositivos;
+	let multiplicacionNegativos;
+	let flag=0;
 	sumaPositivos=0;
 	multiplicacionNegativos=1;
 	respuesta='si';
+	do{
+		numero=parseInt(prompt("Ingrese los numeros que guste"));
+		
+		if(numero>=0){
+		sumaPositivos=sumaPositivos+numero;}
+		else{
+			multiplicacionNegativos=multiplicacionNegativos*numero;	
+			flag=1;
+		}
+		respuesta=prompt("Quiere ingresar otro número? Si así lo desea escriba si y luego ingrese su número");
+	}while(respuesta=='si');
+	if(flag==0){
+		multiplicacionNegativos=0;}
+		document.getElementById("txtIdProducto").value=multiplicacionNegativos;
+	document.getElementById("txtIdSuma").value=sumaPositivos;
+	}	
+	
+	
+
+	
 
 
-	txtIdSuma.value=sumaPositivos;
-	txtIdProducto.value=multiplicacionNegativos;
+	
 
-}//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,21 +1,30 @@
 /*
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
-function mostrar()
-{	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
-	{
-		
-		respuesta=prompt("desea continuar?");
+function mostrar(){
+let numero;
+let seguir;
+let maximo;
+let minimo;
+let flag=0;
+do{
+	numero = parseInt(prompt("Ingrese un número: "));
+	
+	if(flag == 0){
+		numero = maximo;
+		numero = minimo;
+		flag = 1;
 	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
-}//FIN DE LA FUNCIÓN
+	if(numero > maximo){
+		maximo = numero;
+	}
+	else if(numero < minimo){
+		minimo = numero;
+	}
+	
+	seguir = prompt("Quiere ingresar otro número? Si así lo desea escriba si y luego ingrese su número");
+    
+}while(seguir == "si");
+document.getElementById("txtIdMaximo").value = maximo;
+document.getElementById("txtIdMinimo").value = minimo;
+}
